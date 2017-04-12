5986873729936bfce772d5c049c8518eaeaff188
Cvetomir Denchev <cvetomir@localhost.localdomain>
2017-04-12 12:26:38 +0300

    updated person

--- person_student_professor.cpp
+++ person_student_professor.cpp
@@ -12,7 +12,7 @@ class Person {
         virtual void getdata() {
             std::cin >> this->name >> this->age;
         }
-        virtual int putdata() {
+        virtual void putdata() {
             std::cout << this->name << ' ' << this->age;
         }
 };
@@ -27,8 +27,8 @@ class Professor : public Person {
         void getdata() {
             std::cin >> this->name >> this->age >> this->publications;
         }
-        int putdata() {
-            std::cout << this->name << ' ' << this->age << ' ' << this->cur_id << endl;
+        void putdata() {
+            std::cout << this->name << ' ' << this->age << ' ' << this->publications << ' ' << this->cur_id << endl;
         }
         Professor() {
             Professor::professorCount++;
@@ -40,6 +40,7 @@ int Professor::professorCount = 0;
 
 class Student : public Person {
     public:
+        int marksCount = 6;
         int marks[6];
         int cur_id;
         static int studentCount;
@@ -47,25 +48,38 @@ class Student : public Person {
             Student::studentCount++;
             this->cur_id = Student::studentCount;
         }
+        void getdata() {
+            std::cin >> this->name >> this->age >> this->marks[0] >> this->marks[1] >> this->marks[2] >> this->marks[3]
+                     >> this->marks[4] >> this->marks[5];
+        }
+        void putdata() {
+            int total = 0;
+
+            for (int i = 0; i < this->marksCount; i++) {
+                total += marks[i];
+            }
+
+            std::cout << this->name << ' ' << this->age << ' ' << total << ' ' << this->cur_id << endl;
+            
+        }
 };
 
 int Student::studentCount = 0;
 
 int main() {
 
-	Student s;
-	Student p;
+	Student *s = new Student;
+    Student *c = new Student;
+	Student *p = new Student;
 
     Professor *Vasilev = new Professor;
-    Vasilev->getdata();
+    //Vasilev->getdata();
 
     Professor *stamatov = new Professor;
     Professor *Proinov = new Professor;
 
-    // (*a).member and a->member do the same
-    Proinov->getdata();
-    (*Proinov).putdata();
-    // Vasilev->putdata();
+    p->getdata();
+    p->putdata();
 
 	return 0;
 
