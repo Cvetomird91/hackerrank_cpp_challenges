e123a4a96f4b5532df5b17b4c79c8b2d08a33230
Cvetomird91 <cvetomirdenchev@gmail.com>
2017-04-25 11:20:06 +0300

    fixed indentation of maps.cpp

--- maps.cpp
+++ maps.cpp
@@ -15,36 +15,35 @@ const int printMark = 3;
 
 int main() {
 
-	int n;
+    int n;
     map<string, int> marks;
-	string query;
+    string query;
 
     cin >> n;
     cin.ignore(1, '\n');
 	
-	for (int i = 0; i < n; i++) {
-		getline(cin, query);
-		stringstream ss(query);
+    for (int i = 0; i < n; i++) {
+        getline(cin, query);
+        stringstream ss(query);
 
-		int type, grade;
-		string student;
+        int type, grade;
+        string student;
 
-		ss >> type >> student >> grade;
+        ss >> type >> student >> grade;
 
-		if (type == insert) {
-			//marks.insert(make_pair(student, grade));
-			marks[student] += grade;
-		}
+        if (type == insert) {
+            marks[student] += grade;
+        }
 
-		if (type == insertStudent) {
-			marks[student] = 0;
-		}
+        if (type == insertStudent) {
+            marks[student] = 0;
+        }
 
-		if (type == printMark) {
-			cout << marks[student] << endl;
-		}
+        if (type == printMark) {
+            cout << marks[student] << endl;
+        }
 
-	}
+    }
 
     return 0;
 
