5215a041bd3b42f91af93c1104561751c0d877db
Cvetomird91 <cvetomirdenchev@gmail.com>
2017-05-22 17:06:10 +0300

    fixed indentation and removed unnecessary include directives in Student class

--- student_class.cpp
+++ student_class.cpp
@@ -1,9 +1,5 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <cassert>
+
 using namespace std;
 
 const int gradesCount = 5;
@@ -13,19 +9,19 @@ class Student {
 	public:
 		int scores[gradesCount];
 		void input() {
-            int n;
-            for (int i = 0; i < gradesCount; ++i) {
-                cin >> n;
-                this->scores[i] = n;
-            }
+			int n;
+			for (int i = 0; i < gradesCount; ++i) {
+				cin >> n;
+				this->scores[i] = n;
+			}
 		}
 		int calculateTotalScore() {
-            int total = 0; 
-            for (int i = 0; i < gradesCount; ++i) {
-                total += this->scores[i];
-            }
-            
-            return total;
+			int total = 0; 
+			for (int i = 0; i < gradesCount; ++i) {
+				total += this->scores[i];
+			}
+
+			return total;
 		}
 
 };
@@ -45,29 +41,29 @@ int main() {
 //мейна майна:
 
 int main() {
-    int n; // number of students
-    cin >> n;
-    Student *s = new Student[n]; // an array of n students
-    
-    for(int i = 0; i < n; i++){
-        s[i].input();
-    }
+	int n; // number of students
+	cin >> n;
+	Student *s = new Student[n]; // an array of n students
+		
+	for(int i = 0; i < n; i++){
+		s[i].input();
+	}
 
-    // calculate kristen's score
-    int kristen_score = s[0].calculateTotalScore();
+	// calculate kristen's score
+	int kristen_score = s[0].calculateTotalScore();
 
-    // determine how many students scored higher than kristen
-    int count = 0; 
-    for(int i = 1; i < n; i++){
-        int total = s[i].calculateTotalScore();
-        if(total > kristen_score){
-            count++;
-        }
-    }
+	// determine how many students scored higher than kristen
+	int count = 0; 
+	for(int i = 1; i < n; i++){
+		int total = s[i].calculateTotalScore();
+		if(total > kristen_score){
+			count++;
+		}
+	}
 
-    // print result
-    cout << count;
-    
-    return 0;
+	// print result
+	cout << count;
+
+	return 0;
 
 }
