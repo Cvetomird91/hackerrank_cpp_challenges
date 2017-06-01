acec73fb3eab04586d343681d699957b1d163fdb
Cvetomird91 <cvetomirdenchev@gmail.com>
2017-06-01 16:30:43 +0300

    updated concat arrays

--- concat_arrays.cpp
+++ concat_arrays.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 
-template <typename T>
-int* concatArrays( T& array_1, T& array_2) {
-	int arrayCount_1 = sizeof(array_1) / sizeof(array_1[0]);
-	int arrayCount_2 = sizeof(array_2) / sizeof(array_2[0]);
-	int newArraySize = arrayCount_1 + arrayCount_2;
+using namespace std;
 
-	int *p = new int[newArraySize];
+template <typename T1, typename T2>
+int* concatArrays( T1& array_1, T2& array_2) {
+  int arrayCount_1 = sizeof(array_1) / sizeof(array_1[0]);
+  int arrayCount_2 = sizeof(array_2) / sizeof(array_2[0]);
+  int newArraySize = arrayCount_1 + arrayCount_2;
 
-	for (int i; i < arrayCount_1; i++)
-		p[i] = array_1[i];
+  int *p = new int[newArraySize];
 
-	for (int i = arrayCount_1; i < (arrayCount_1 + arrayCount_2); i++)
-		p[i] = array_2[i-arrayCount_2];
+  for (int i = 0; i < arrayCount_1; i++) {
+  	p[i] = array_1[i];
+  }
 
-	return p;
+  for (int i = arrayCount_1; i < newArraySize; i++) {
+	int newIndex = i-arrayCount_2;
+	p[i] = array_2[--newIndex];
+  }
 
+  return p;
 }
 
 int main() {
+  int ary[4] = {1, 2, 3, 123};
+  int anotherAry[3] = {4, 5, 6};
 
-	int ary[3] = {1, 2, 3};
-	int anotherAry[3] = {4, 5, 6};
+  int *r = concatArrays(ary, anotherAry);
 
-	int *r = concatArrays(ary, anotherAry);
+  cout << *(r + 0) << endl;
+  cout << *(r + 1) << endl;
+  cout << *(r + 2) << endl;
+  cout << *(r + 3) << endl;
+  cout << *(r + 4) << endl;
+  cout << *(r + 5) << endl;
+  cout << *(r + 6) << endl;
 
-	std::cout << *(r + 4) << std::endl;
+  delete r;
 
-	delete r;
-
-	return 0;
-
+  return 0;
 }
